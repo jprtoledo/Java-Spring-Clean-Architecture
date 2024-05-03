7e3c8a958f3d14d515f5abfb62fe6a90e9cd0663
jprtoledo <juan.prtoledo@gmail.com>
2024-05-03 09:40:40 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java Spring Clean Architecture
+
+This is my implementation of a REST API using Spring, applying Clean Architecture techniques. I use this project as a base to new projects I need to develop.
---
+++ src/main/java/br/com/jprtoledo/javaspringcleanarchitecture/JavaSpringCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package br.com.jprtoledo.javaspringcleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringCleanArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringCleanArchitectureApplication.class, args);
+    }
+
+}
